ff66272aa4e3ad316adf994ea5b71783af136103
Doğa Savaş <doasavaas@gmail.com>
2024-10-11 16:17:32 +0300

    sidenavda ve login page.jsxte svgler yeniden çağırıldı

--- components/sidenav/index.jsx
+++ components/sidenav/index.jsx
@@ -1,17 +1,19 @@
+import Logo from "@/svgs/logo"
 import "./sidenav.css"
 import signOut from "@/actions/auth"
+import LogOut from "@/svgs/logout"
 
 export default function Sidenav() {
   return (
     <div className="sidenav">
       <div className="sidenavElements">
-      {/* <Logo /> */}
+         <Logo />
         <a href="#">Profil</a>
         <a href="#">Not bilgileri</a>
         <a href="#">Ayarlar</a>
       </div>
       <form className="sidenavlogout" action={signOut}>
-        {/* <LogOut /> */}
+        <LogOut /> 
         <button>Log Out</button>
       </form>
     </div>
