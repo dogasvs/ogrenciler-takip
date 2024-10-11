baec9d1012c841c8de0661e71d70d35d60d7fb59
Doğa Savaş <doasavaas@gmail.com>
2024-10-11 14:46:34 +0300

    login.jsx dosyası oluşturuldu

---
+++ app/login/page.jsx
@@ -0,0 +1,33 @@
+import Logo from '@/svgs/logo/page';
+import { login } from './action'
+import "./login.css";
+import Github from '@/svgs/github/page';
+import Google from '@/svgs/google/page';
+import Facebook from '@/svgs/facebook/page';
+import Link from 'next/link';
+import { redirect } from 'next/navigation';
+
+export default function LoginPage() {
+  return (
+   <form className="loginForm">
+      <div className="logo">
+        <Logo /> 
+        <h1>Giriş Yap</h1>
+      </div>
+      <label htmlFor="email">E-posta</label>
+      <input id="email" name="email" type="email" required placeholder='username@gmail.com' />
+      <label htmlFor="password">Şifre</label>
+      <input id="password" name="password" type="password" required placeholder='*******' />
+      <button className='loginBtn' formAction={login}>Giriş yap</button>
+      <p>Ya da hesaplarla devam edin</p>
+      <div className="withSocialMedia">
+        <Google />
+        <Github />
+        <Facebook />
+      </div>
+      <div className="orSignUp">
+        <p>Hesabın Yok Mu? <Link href={"/sign-up"} className='kaydolBtn'>Kaydol</Link> </p>
+      </div>
+    </form>
+  );
+}
